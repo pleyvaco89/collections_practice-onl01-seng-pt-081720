63f15135da5b61a3711a709fb6765533281d956f
Priscilla Leyva <pleyva.co89@gmail.com>
2020-08-26 03:11:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
-
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort.reverse
+end
+def sort_array_char_count(array)
+  array.sort_by {|el| el.length}
+end
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+def reverse_array(array)
+  array.reverse
+end
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
+  end
+end
+def find_a(array)
+  array.select {|name| name[0] == "a"}
+end
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
+def add_s(array)
+  array.collect.with_index do |word, index|
+    if index != 1
+      word + "s"
+    else
+      word
+    end
+  end
+end
