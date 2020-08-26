9a34a1d58d357150a1381293d747502d05777ed9
Priscilla Leyva <pleyva.co89@gmail.com>
2020-08-26 03:11:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,119 +1,36 @@
-def sort_array_asc (array)
+def sort_array_asc(array)
   array.sort
 end
-
-
-
-def sort_array_desc (array)
-  array.sort {|x,y| y <=> x }
+def sort_array_desc(array)
+  array.sort.reverse
 end
-
-
-
-def swap_elements (array)
+def sort_array_char_count(array)
+  array.sort_by {|el| el.length}
+end
+def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
-
-
-
-def reverse_array (array)
+def reverse_array(array)
   array.reverse
 end
-
-
-
-def kesha_maker (array)
-  array.collect do |element|
-    element[2] = "$"
-    element
+def kesha_maker(array)
+  array.each do |name|
+    name[2] = "$"
   end
 end
-
-
-
-def greater_and_less_than_10 (array)
-  hash = {
-    "greater_than_10" => [],
-    "less_than_10" => []
-  }
-  array.each do |number|
-    if number > 10
-      hash["greater_than_10"] << number
-    else
-      hash["less_than_10"] << number
-    end
-  end
-  hash
+def find_a(array)
+  array.select {|name| name[0] == "a"}
 end
-
-
-
-def find_winners (hash)
-  winners = []
-  hash.each do |name, status|
-    winners << name if status == "winner"
-  end
-  winners
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
 end
-
-
-
-def find_a (array)
-  array.select do |word|
-    word.chr == "a"
-  end
-end
-
-
-
-def sum_array (array)
-  array.inject do |sum, num|
-    sum + num
-  end
-end
-
-
-
-def add_s (array)
+def add_s(array)
   array.collect.with_index do |word, index|
-    if index == 1
-      word
-    else
+    if index != 1
       word + "s"
+    else
+      word
     end
   end
 end
-
-
-
-def count_words (words)
-  story_count = Hash.new
-
-  words.split.each do |word|
-    if story_count.include?(word)
-      story_count[word] = story_count[word] + 1
-    else
-      story_count[word] = 1
-    end
-  end
-
-  story_count
-end
-
-
-
-def organize_songs_by_artist (tracks)
-  hash = {}
-  tracks.each do |name|
-    artist = name.split(" - ")[0]
-    trackname = name.split(" - ")[1]
-
-    if hash.include?(artist)
-      hash[artist] << trackname
-    else
-      hash[artist] = [] << trackname
-    end
-  end
-  hash
-end  
